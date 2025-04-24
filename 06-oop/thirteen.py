1fe4bf2f0d7e276e0bbbc7e148b962c148bd35dd
Tabsheera Shakeel <164553407+Tabsheera-Shakeel@users.noreply.github.com>
2025-04-23 17:16:25 -0700

    Add files via upload

---
+++ 06-oop/thirteen.py
@@ -0,0 +1,13 @@
+class Engine:
+    def start(self):
+        print("Engine started.")
+
+class Car:
+    def __init__(self, engine):
+        self.engine = engine
+
+    def start_car(self):
+      self.engine.start()        
+
+          
+          
